74612c54713387e04d00c2f5c734f964a3fd3836
Zsolesz <72067999+gazdi435@users.noreply.github.com>
2024-02-23 11:02:40 +0100

    Add files via upload

--- verseny/verseny/Program.cs
+++ verseny/verseny/Program.cs
@@ -20,26 +20,20 @@ namespace verseny
             //a
             /*A fájlban tárolt számok között hány olyan szerepel, amely a 1310438493 számmal relatív prím?
             Két számot relatív prímnek nevezünk, ha legnagyobb közös osztójuk 1.
-            Pl.: a 10 és a 21 relatív prímek (bár egyik sem prímszám).
-            Beküldendő egy szám (a válasz a kérdésre)*/
+            Pl.: a 10 és a 21 relatív prímek (bár egyik sem prímszám). xDDDD
+            Beküldendő egy szám (a válasz a kérdésre) */
             int szam = 1310438493;
-            List<int> osztok = new List<int>();
-            // megadott szam LNKO-ja
-            while (szam > 1)
+            List<long> relativ_primek = new List<long>();
+            List<long> szamosztoi = osztok(szam);
+            foreach( long l in list )
             {
-                for (int i = 2; i < szam + 1; i++)//bananá
-                {
-                    if (szam % i == 0)
-                    {
-                        Console.WriteLine($"dibag: {szam} % {i} = {szam % i}");
-                        szam = szam / i;
-                        osztok.Add(i);
-                        break;
-                    }
+                List<long> vizsgaltszam_osztoi = osztok(l);
+                if (!szamosztoi.Intersect(vizsgaltszam_osztoi).Any()) {
+                    relativ_primek.Add(l);
+                    Console.WriteLine($"relativ prim: {l}");
                 }
             }
-            Console.WriteLine($"Legnagyobb osztó: {osztok.Max()}");
-
+            Console.WriteLine($"{szam} relatív prímei ({relativ_primek.Count})");
 
 
             //b
@@ -48,6 +42,34 @@ namespace verseny
             Console.WriteLine(list.Distinct().ToList().Count(x => Ellenoriz(x)));
 
 
+            //2. feladatat
+            //a)
+            List<List<string>> megyekList = File.ReadAllLines("telepules.txt").Select(x=> x.Split("").ToList()).ToList();
+            //Dictionary<string, string> megyekDic = File.ReadAllLines("megyek.txt").Select(x => x.Split("").ToDictionary(string,string)).ToList();
+
+            List<string> megoldas = megyekList.OrderByDescending(x => Convert.ToInt16(x[5])).ToList()[1];
+            Console.WriteLine(megyekList.OrderByDescending(x => Convert.ToInt16(x[5])).ToList()[1]);
+
+
+
+        }
+        public static List<long> osztok(long szam)
+        {
+            List<long> osztok_list = new List<long>();
+            while (szam > 1)
+            {
+                for (int i = 2; i < szam + 1; i++)//bananá
+                {
+                    if (szam % i == 0)
+                    {
+                        //Console.WriteLine($"dibag: {szam} % {i} = {szam % i}");
+                        szam = szam / i;
+                        osztok_list.Add(i);
+                        break;
+                    }
+                }
+            }
+            return osztok_list;
         }
 
         public static bool Ellenoriz(long adat)
